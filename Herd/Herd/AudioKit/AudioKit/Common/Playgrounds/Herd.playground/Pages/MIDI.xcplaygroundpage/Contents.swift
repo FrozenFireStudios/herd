16f22ceeb5a2b4ce299c7ed5dde8b2ed1017c3a4
Aurelius Prochazka <aure@aure.com>
2016-08-24 00:51:49 -0600

    More sounds

--- Herd/Herd/AudioKit/AudioKit/Common/Playgrounds/Herd.playground/Pages/MIDI.xcplaygroundpage/Contents.swift
+++ Herd/Herd/AudioKit/AudioKit/Common/Playgrounds/Herd.playground/Pages/MIDI.xcplaygroundpage/Contents.swift
@@ -5,8 +5,8 @@ import AudioKit
 
 //: Create some samplers, load different sounds, and connect it to a mixer and the output
 var sampler = AKSampler()
-sampler.loadWav("bleat")
-sampler.tuning = 12
+sampler.loadWav("baa")
+sampler.tuning = -12
 var mixer = AKMixer(sampler)
 
 let reverb = AKCostelloReverb(mixer)
@@ -16,7 +16,7 @@ AudioKit.output = AKBooster(dryWetmixer, gain: 10)
 
 //: Create the sequencer after AudioKit's output has been set
 //: Load in a midi file, and set the sequencer to the main audiokit engine
-var sequencer = AKSequencer(filename: "beethoven_les_adieux_1", engine: AudioKit.engine)
+var sequencer = AKSequencer(filename: "smwintro", engine: AudioKit.engine)
 
 
 AudioKit.start()
