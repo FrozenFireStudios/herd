e95ed62af8e817e5bd5e8b649d7a0e77f794385e
Ryan Poolos <ryanpoolos@Gmail.com>
2016-08-24 01:03:53 -0600

    Basic 3D stuff is working at 90 degree angle, top down.

--- Herd/Herd/GameViewController.swift
+++ Herd/Herd/GameViewController.swift
@@ -12,8 +12,8 @@ import SceneKit
 
 class GameViewController: UIViewController {
     
-    var scnView: SCNView? {
-        return view as? SCNView
+    var scnView: SCNView {
+        return view as! SCNView
     }
     
     override func loadView() {
@@ -23,92 +23,33 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        view.backgroundColor = UIColor.lightGrayColor()
+        
+        let scene = SCNScene()
         
         // Set up the camera
         scene.rootNode.addChildNode(cameraNode)
         
-        // create and add a light to the scene
+        // This is a really lazy light source. But its kinda sun like...
         let lightNode = SCNNode()
         lightNode.light = SCNLight()
-        lightNode.light!.type = SCNLightTypeOmni
-        lightNode.position = SCNVector3(x: 0, y: 50, z: 0)
-        cameraNode.addChildNode(lightNode)
+        lightNode.light?.type = SCNLightTypeOmni
+        lightNode.position = SCNVector3(x: 0, y: 25, z: 0)
+        scene.rootNode.addChildNode(lightNode)
         
-        // create and add an ambient light to the scene
-        let ambientLightNode = SCNNode()
-        ambientLightNode.light = SCNLight()
-        ambientLightNode.light!.type = SCNLightTypeAmbient
-        ambientLightNode.light!.color = UIColor.darkGrayColor()
-        scene.rootNode.addChildNode(ambientLightNode)
+        // Add our scene to the view
+        scnView.scene = scene
+        scnView.showsStatistics = true
         
-        // retrieve the ship node
-        let ship = scene.rootNode.childNodeWithName("ship", recursively: true)!
-        
-        // animate the 3d object
-        ship.runAction(SCNAction.repeatActionForever(SCNAction.rotateByX(0, y: 2, z: 0, duration: 1)))
-        
-        scnView?.scene = scene
-        scnView?.showsStatistics = true
-        scnView?.backgroundColor = UIColor.greenColor()
-        
-        // add a tap gesture recognizer
+        // Setup some Gestures
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
         view.addGestureRecognizer(tapGesture)
         
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
+        view.addGestureRecognizer(panGesture)
+        
         let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(handlePinch(_:)))
         view.addGestureRecognizer(pinchGesture)
-    }
-    
-    func handleTap(gestureRecognize: UIGestureRecognizer) {
-        let point = gestureRecognize.locationInView(view)
-        
-        guard let scnView = scnView else {
-            return
-        }
-        
-        let hitResults = scnView.hitTest(point, options: nil)
-        
-        // check that we clicked on at least one object
-        if hitResults.count > 0 {
-            // retrieved the first clicked object
-            let result: AnyObject! = hitResults[0]
-            
-            // get its material
-            let material = result.node!.geometry!.firstMaterial!
-            
-            // highlight it
-            SCNTransaction.begin()
-            SCNTransaction.setAnimationDuration(0.5)
-            
-            // on completion - unhighlight
-            SCNTransaction.setCompletionBlock {
-                SCNTransaction.begin()
-                SCNTransaction.setAnimationDuration(0.5)
-                
-                material.emission.contents = UIColor.blackColor()
-                
-                SCNTransaction.commit()
-            }
-            
-            material.emission.contents = UIColor.redColor()
-            
-            SCNTransaction.commit()
-        }
-        else {
-            let boxGeometry = SCNBox(width: 2.0, height: 2.0, length: 2.0, chamferRadius: 0.0)
-            
-            // The zeropoint for the z isn't actually even, so grab our origin.
-            let projectedOrigin = scnView.projectPoint(SCNVector3Zero)
-            let touchPoint = SCNVector3(x: Float(point.x), y: Float(point.y), z: projectedOrigin.z)
-            let convertedPoint = scnView.unprojectPoint(touchPoint)
-            
-            let box = SCNNode(geometry: boxGeometry)
-            box.position = convertedPoint
-            scnView.scene?.rootNode.addChildNode(box)
-            
-            box.runAction(SCNAction.repeatActionForever(SCNAction.rotateByX(2, y: 2, z: 2, duration: 1)))
-        }
     }
     
     override func shouldAutorotate() -> Bool {
@@ -128,18 +69,27 @@ class GameViewController: UIViewController {
     }
     
     //==========================================================================
-    // MARK: - Camera
+    // MARK: - Gestures
     //==========================================================================
     
-    lazy var cameraNode: SCNNode = {
-        let cameraNode = SCNNode()
-        cameraNode.camera = SCNCamera()
+    var touchedNode: SCNNode?
+    func handlePan(gesture: UIPanGestureRecognizer) {
+        let point = gesture.locationInView(view)
         
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 25)
-//        cameraNode.eulerAngles = SCNVector3(x: -Float(M_PI_2), y: 0.0, z: 0.0)
+        if gesture.state == .Began {
+            let hitResults = scnView.hitTest(point, options: nil)
+            touchedNode = hitResults.first?.node
+        }
         
-        return cameraNode
-    }()
+        if gesture.state == .Changed {
+            touchedNode?.position = convert2Dto3D(point)
+        }
+    }
+    
+    func handleTap(gesture: UITapGestureRecognizer) {
+        let point = gesture.locationInView(view)
+        addPyramid(atPoint: point)
+    }
     
     var startingPosition: SCNVector3 = SCNVector3Zero
     func handlePinch(gesture: UIPinchGestureRecognizer) {
@@ -150,11 +100,66 @@ class GameViewController: UIViewController {
             let zFar: Float = Float(cameraNode.camera?.zFar ?? 100.0)
             let buffer: Float = 10.0
             
-            let startPoint = startingPosition.z
-            var z = startPoint + (startPoint - (startPoint * Float(gesture.scale)))
-            z = min(z, zFar - buffer)
-            z = max(z, buffer)
-            cameraNode.position.z = z
+            let startAxis = startingPosition.y
+            var endAxis = startAxis + (startAxis - (startAxis * Float(gesture.scale)))
+            endAxis = min(endAxis, zFar - buffer)
+            endAxis = max(endAxis, buffer)
+            cameraNode.position.y = endAxis
         }
+    }
+    
+    //==========================================================================
+    // MARK: - Models
+    //==========================================================================
+    
+    func addBox(atPoint point: CGPoint) {
+        let geometry = SCNBox(width: 2.0, height: 2.0, length: 2.0, chamferRadius: 0.0)
+        
+        let node = SCNNode(geometry: geometry)
+        node.position = convert2Dto3D(point)
+        scnView.scene?.rootNode.addChildNode(node)
+        
+        node.runAction(SCNAction.repeatActionForever(SCNAction.rotateByX(0, y: 2, z: 0, duration: 1)))
+    }
+    
+    func addPyramid(atPoint point: CGPoint) {
+        let geometry = SCNPyramid(width: 2.0, height: 2.0, length: 2.0)
+        
+        let node = SCNNode(geometry: geometry)
+        node.position = convert2Dto3D(point)
+        scnView.scene?.rootNode.addChildNode(node)
+        
+        node.runAction(SCNAction.repeatActionForever(SCNAction.rotateByX(0, y: 2, z: 0, duration: 1)))
+    }
+    
+    //==========================================================================
+    // MARK: - Camera
+    //==========================================================================
+    
+    lazy var cameraNode: SCNNode = {
+        let cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        
+        cameraNode.position = SCNVector3(x: 0, y: 25, z: 0)
+        cameraNode.eulerAngles = SCNVector3(x: -Float(M_PI_2), y: 0.0, z: 0.0)
+        
+        return cameraNode
+    }()
+    
+    //==========================================================================
+    // MARK: - Coordinates
+    //==========================================================================
+    
+    func convert2Dto3D(point: CGPoint) -> SCNVector3 {
+        // The zeropoint for the z isn't actually even, so grab our origin.
+        let projectedOrigin = scnView.projectPoint(SCNVector3Zero)
+        
+        // Convert to 3D with our x/y plus the origin z
+        let touchPoint = SCNVector3(x: Float(point.x), y: Float(point.y), z: projectedOrigin.z)
+        
+        // unproject it to 3D space
+        let convertedPoint = scnView.unprojectPoint(touchPoint)
+        
+        return convertedPoint
     }
 }
